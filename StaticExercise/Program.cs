157b88cc0d5483f1587c7cc4f8a1d18dfcd8d72e
Ethan Jacobs <jacobethan1992@gmail.com>
2023-02-16 15:11:22 -0500

    initial commit

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace StaticExercise
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(TempConverter.FahrenheitToCelsius(32));
+            Console.WriteLine(TempConverter.CelsiusToFahrenheit(100));
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    internal static class TempConverter
+
+    {
+        public static double FahrenheitToCelsius(double num)
+        {
+            return (num - 32) * .555;
+        }
+        public static double CelsiusToFahrenheit(double num)
+        {
+            return (num * 1.8) + 32;
+        }
+    }
+
+    }
